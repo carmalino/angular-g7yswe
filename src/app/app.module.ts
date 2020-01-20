ea951a8b1c15934e2ed7a8d274bcbe2a22645723
carmalino <35498882+carmalino@users.noreply.github.com>
2020-01-20 09:30:27 -0500

    Add files via upload

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,26 @@
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
+import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { HelloComponent } from './hello.component';
+import { HeaderComponent } from './components/header/header.component';
+import { BodyComponent } from './components/body/body.component';
+
+
+
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
-  bootstrap:    [ AppComponent ]
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    BodyComponent
+  ],
+  //modulos
+  imports: [
+    BrowserModule
+  ],
+  //servicios inyectados
+  providers: [],
+  //componente principal
+  bootstrap: [AppComponent]
 })
 export class AppModule { }
